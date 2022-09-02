9d10ca63b061dd83802607a46f5c0da6f859a124
woonys <wodns1324@naver.com>
2022-09-02 17:50:15 +0900

    컨트롤러: withOutString 메소드 추가

--- ControllerConvertStringToLocalDateTimeExample/src/main/java/com/woony/core/controller/UpdateDateController.java
+++ ControllerConvertStringToLocalDateTimeExample/src/main/java/com/woony/core/controller/UpdateDateController.java
@@ -1,14 +1,15 @@
 package com.woony.core.controller;
 
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.woony.core.domain.request.UpdateDateRequestWithString;
+import com.woony.core.domain.request.UpdateDateRequestWithoutString;
 import com.woony.core.service.UpdateDateService;
 
 @RestController
@@ -21,10 +22,17 @@ public class UpdateDateController {
         this.updateDateService = updateDateService;
     }
     // Case 1: String -> Date 변환 로직 포함한 기능 구현
-    @PutMapping("/updateDate/{dateId}")
+    @PutMapping("/updateDateWithString/{dateId}")
     public String updateDateFromString(@PathVariable long dateId, @RequestBody UpdateDateRequestWithString request) {
         String fetchDate = request.getDateAsString();
-        updateDateService.updateCurrentDate(dateId, fetchDate);
+        updateDateService.updateCurrentDateWithString(dateId, fetchDate);
+        return "Success";
+    }
+
+    @PutMapping("/updateDateWithoutString/{dateId}")
+    public String updateDateWithoutString(@PathVariable long dateId, @RequestBody UpdateDateRequestWithoutString request) {
+        LocalDateTime fetchDate = request.getDateWithLDT();
+        updateDateService.updateCurrentDateWithoutString(dateId, fetchDate);
         return "Success";
     }
 }
